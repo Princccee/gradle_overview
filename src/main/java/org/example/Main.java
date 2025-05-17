ad830d2342d9197113a4304c3a4ffbbe859ef7f1
Prince <princekumaraman7654@gmail.com>
2025-05-17 15:12:18 +0530

    make an HTTPS request using okhttp

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,11 +2,32 @@ package org.example;
 
 import okhttp3.OkHttp;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+//        System.out.println("Hello, World!");
 
         OkHttpClient client = new OkHttpClient();
+
+        String url = "https://jsonplaceholder.typicode.com/todos/1";
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try(Response response = client.newCall(request).execute()){
+            if(!response.isSuccessful()){
+                System.out.println("Something went wrong");
+            }
+            System.out.println(response.body().string());
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+
     }
 }

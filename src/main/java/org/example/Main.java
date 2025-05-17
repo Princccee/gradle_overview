6ca9f684bfc01a9fa68471c07d977447e06fbbe7
Prince <princekumaraman7654@gmail.com>
2025-05-17 13:48:21 +0530

    initialise gradle project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import okhttp3.OkHttp;
+import okhttp3.OkHttpClient;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        OkHttpClient client = new OkHttpClient();
+    }
+}
